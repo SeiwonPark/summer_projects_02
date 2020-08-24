1def2be71aed3ddbfa44924d04aaccd84066a2b3
SeiwonPark <63793178+SeiwonPark@users.noreply.github.com>
2020-08-24 17:10:07 +0900

    Update ServoControl.ino

--- sensors/ServoControl.ino
+++ sensors/ServoControl.ino
@@ -1,15 +1,17 @@
+// servo control test
+
 #include <Servo.h>
 
 Servo servo;
 
 void setup() {
   // put your setup code here, to run once:
-  myservo.attach(3) //  3번 디지털 핀에 서보모터를 연결
+  myservo.attach(3) //  connect servo with digital pin 3
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  for (int i = 0; i < 170; i += 10) {   // 딜레이 500ms(0.5초) 마다 10도씩 회전
+  for (int i = 0; i < 170; i += 10) {
     servo.write(i);
     delay(500);                        
   }
